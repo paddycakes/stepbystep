2a21da4dbfd10c476e64c1f6526e85893ed5e889
Patrick Gallagher <paddy.gallagher@gmail.com>
2016-03-19 20:16:16 +0000

    Reload browser when any jsx files are modified

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var config = {
   devBaseUrl: 'http://localhost',
   paths: {
     html: './src/*.html',
-    js: './src/**/*.js',
+    js: ['./src/**/*.js', './src/**/*.jsx'],
     assets: './assets//**/*',
     dist: './dist',
     mainJs: './src/index.js'
